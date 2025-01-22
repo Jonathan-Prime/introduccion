319946d5961abc79c4df63c69cfe81add3778ef3
Camilopelos <juanca36823@gmail.com>
2025-01-21 22:43:01 -0500

    Cambios 1

--- prueba/src/App.js
+++ prueba/src/App.js
@@ -60,36 +60,7 @@ function App() {
           <li className="options">Menu</li>
         </ul>
       </nav>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button
-          className="button"
-          onClick={(value) => funcionbutton((value = "123"))}
-        >
-          Cambiar opiones de footer
-        </button>
-
-        <button
-          className="button"
-          onClick={(number) => actualizar(increment)}
-        >
-         Actualizar
-        </button>
-        <p>
-          {increment}
-        </p>
-      </header>
+      
       <Formulario />
       <Component footerOptions={opcionesFooter} numberSecond={increment} actualizar={actualizar} />
     </div>
--- prueba/src/Form.js
+++ prueba/src/Form.js
@@ -2,17 +2,59 @@ import "./assets/component.css";
 import React,{useState} from"react"
 function Formulario() {
 const [formData,setFormData] = useState ({
-    nombre: "",
-    cedula: "",
+    name: "",
     email: "",
+    password: "",
 })
 const isFormIncomplete =
-    !formData.nombre || !formData.email || !formData.cedula;
+    !formData.name || !formData.email || !formData.password;
    const handleSubmit= (event) => { 
 event.preventDefault();
-    alert(`Datos enviados:\nNombre: ${formData.nombre}\nCorreo: ${formData.email}\nCédula: ${formData.cedula}`);
-   }
 
+create({
+    name: formData.name,
+    email: formData.email,
+    password: formData.password,
+  });
+    
+   }
+   async function create(blogPost) {
+    try {
+      // Create the URL
+      const url = "https://server-dw5y.onrender.com/api/auth/register";
+  
+      // Create the headers
+      const headers = {
+        "Content-Type": "application/json",
+      };
+  
+      // Create the POST body
+      const body = JSON.stringify(
+        blogPost)
+        ;
+  console.log (body)
+      // Send the POST request
+      const response = await fetch(url, { method: "POST", headers, body });
+      console.log(response);
+      alert("Success:");
+      formData.email=""
+      formData.name=""
+      formData.password=""
+      // Check the response status
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      // Parse the JSON response
+      const data = await response.json();
+      
+    } catch (error) {
+      // Handle any errors
+      alert("Error:");
+    }
+  }
+  
+  
 
 const handleChange= (event) => {
     const {name,value}=event.target
@@ -26,11 +68,11 @@ const handleChange= (event) => {
 return ( 
     <form action="hola" onSubmit={handleSubmit}>
         <label>nombre </label>
-        <input onChange= {handleChange} type="text" name= "nombre" value={formData.nombre}/> 
-        <label>numero </label>
-        <input onChange= {handleChange} type="number" name= "cedula" value={formData.cedula}/>
-        <label>email </label>
-        <input onChange= {handleChange} type="email" name= "email"value={formData.email}/>
+        <input onChange= {handleChange} type="text" name= "name" value={formData.name}/> 
+        <label> correo </label>
+        <input onChange= {handleChange} type="text" name= "email" value={formData.email}/>
+        <label>contraseña </label>
+        <input onChange= {handleChange} type="password" name= "password"value={formData.password}/>
         <button disabled={isFormIncomplete} type ="submit">Enviar</button> 
     </form>
 );
@@ -39,3 +81,5 @@ return (
 }
 
 export default Formulario;
+
+
