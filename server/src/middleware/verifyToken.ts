98b6be5a89fb3caaf38ac8aeb34f5f0f86389df6
Jonme Prime <jonathanjdml@hotmail.com>
2025-01-22 22:15:13 -0500

    bearer toke

--- server/src/middleware/verifyToken.ts
+++ server/src/middleware/verifyToken.ts
@@ -17,16 +17,28 @@ declare global {
 
 const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    let token = req?.headers?.authorization?.split(`"`)[1];
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    // Extraer el token eliminando el prefijo "Bearer "
+    const token = authHeader.split(" ")[1];
     if (!token) return res.status(401).send({ message: "Unauthorized" });
-    let decoded = await verify(token, process.env.JWT_SECRET as string);
-    console.log(process.env.JWT_SECRET)
+
+    // Verificar el token
+    const decoded = await verify(token, process.env.JWT_SECRET as string);
+    console.log(process.env.JWT_SECRET);
+
+    // Asignar el usuario decodificado al objeto req
     req.user = decoded as User;
+
     next();
   } catch (error: any) {
     console.log(error);
-    if (error.message === "jwt expired")
+    if (error.message === "jwt expired") {
       return res.status(401).send({ message: "Unauthorized" });
+    }
 
     res.status(400).send({ message: "Error" });
   }
