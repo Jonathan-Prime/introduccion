ea26b7a04bd97f5d6a07f3507a847c309edc2d2c
Jonme Prime <jonathanjdml@hotmail.com>
2025-01-22 22:04:23 -0500

    add verifitoken

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { loginUser, registerUser, getUsers } from "../controllers/user";
+import verifyToken from "../middleware/verifyToken";
 
 const router = Router();
 
 router.post("/login", loginUser);
 router.post("/register", registerUser);
-router.get("/getUsers", getUsers);
+router.get("/getUsers", verifyToken, getUsers);
 
 export default router;
